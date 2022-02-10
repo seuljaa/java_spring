7edc634870d821b753f00c3b8f62c8d30798a5c3
seuljaa <seuljaa940503@gmail.com>
2022-02-10 17:13:47 +0900

    SQL을 xml 파일로 옮김

--- src/main/java/com/lllolll/example/demo/repository/ArticleRepository.java
+++ src/main/java/com/lllolll/example/demo/repository/ArticleRepository.java
@@ -17,21 +17,15 @@ import com.lllolll.example.demo.vo.Article;
 @Mapper
 public interface ArticleRepository {
 	
-	@Insert("INSERT INTO article SET title= #{title}, `body`= #{body}, updateDate = NOW(), regDate = NOW()")
 	public void writeArticle(@Param("title") String title, @Param("body") String body);
 	
-	@Delete("DELETE FROM article WHERE id= #{id}")
 	public void deleteArticle(@Param("id") int id);
 	
-	@Update("UPDATE article SET title= #{title}, `body`= #{body}, updateDate = NOW() WHERE id = #{id}")
 	public void modifyArticle(@Param("id") int id, @Param("title") String title, @Param("body") String body);
 	
-	@Select("SELECT * FROM article WHERE id= #{id}")
 	public Article showArticle(@Param("id") int id);
 	
-	@Select("SELECT * FROM article ORDER BY title DESC")
 	public List<Article> showArticles();
 	
-	@Select("SELECT COUNT(*) FROM article")
 	public int getLastId();
 }
