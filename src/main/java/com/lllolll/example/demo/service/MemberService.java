47362a64d88e820c04d92967a08f81523f5428e0
seulja <seuljaa940503@gmail.com>
2022-02-14 10:57:37 +0900

    입력데이터 유효성체크, 공백도 체크

--- src/main/java/com/lllolll/example/demo/service/MemberService.java
+++ src/main/java/com/lllolll/example/demo/service/MemberService.java
@@ -5,29 +5,28 @@ import org.springframework.stereotype.Service;
 import com.lllolll.example.demo.repository.MemberRepository;
 import com.lllolll.example.demo.vo.Member;
 
-
 @Service
 public class MemberService {
-	
+
 	private MemberRepository memberRepository;
-	
+
 	public MemberService(MemberRepository memberRepository) {
 		this.memberRepository = memberRepository;
 	}
-	
+
 	public int join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
 		Member oldMember = getMemberByLoginId(loginId);
-		
-		if ( oldMember != null) {
+
+		if (oldMember != null) {
 			return -1;
 		}
-		
+
 		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
 		return memberRepository.getLastId();
 	}
 
 	private Member getMemberByLoginId(String loginId) {
-		return memberRepository.getMemberByLoginId(loginId); 
+		return memberRepository.getMemberByLoginId(loginId);
 	}
 
 	public Member getMemberById(int id) {
