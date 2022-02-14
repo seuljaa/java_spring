37015e14ab3e4e24b4e09439620c8e7008ea908b
seulja <seuljaa940503@gmail.com>
2022-02-14 09:21:15 +0900

    member 테이블 및 테스트데이터 생성

--- db/schema.sql
+++ db/schema.sql
@@ -26,3 +26,49 @@ SET regDate = NOW(),
 updateDate = NOW(),
 title = '제목3',
 `body` = '내용3';
+
+CREATE TABLE `member`(
+id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+regDate DATETIME NOT NULL,
+updateDate DATETIME NOT NULL,
+loginId CHAR(20) NOT NULL,
+loginPw CHAR(60) NOT NULL,
+`authLevel` SMALLINT(2) UNSIGNED DEFAULT 3 COMMENT '권한레벨(3=일반, 7=관리자)',
+`name` CHAR(20) NOT NULL,
+`nickname` CHAR(20) NOT  NULL,
+cellphonNo CHAR(20) NOT NULL,
+email CHAR(50) NOT NULL,
+delStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '탈퇴여부(0=탈퇴전, 1=탈퇴)',
+delDate DATETIME COMMENT '탈퇴날짜'
+);
+
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'admin',
+loginPw = 'admin',
+`authLevel` = 7,
+`name` = '관리자',
+`nickname` = '관리자',
+cellphonNo = '01011112222',
+email = 'seuljaa940503@gmail.com';
+
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'user1',
+loginPw = 'user1',
+`name` = '사용자1',
+`nickname` = '사용자1',
+cellphonNo = '01011112222',
+email = 'seuljaa940503@gmail.com';
+
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'user2',
+loginPw = 'user2',
+`name` = '사용자2',
+`nickname` = '사용자2',
+cellphonNo = '01011112222',
+email = 'seuljaa940503@gmail.com';
